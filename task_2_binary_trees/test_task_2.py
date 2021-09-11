a84c2f892eb9d758b1a13f95816900210462b66f
johnk2280 <johnk2280@gmail.com>
2021-09-11 16:44:59 +0500

    [add] added blanks for tests and classes

---
+++ task_2_binary_trees/test_task_2.py
@@ -0,0 +1,21 @@
+import pytest
+
+from .task_2_binary_trees import BST, BSTFind, BSTNode
+
+
+class TestBinaryTree:
+
+    def test_FindNodeByKey(self):
+        pass
+
+    def test_AddKeyValue(self):
+        pass
+
+    def FinMinMax(self):
+        pass
+
+    def test_DeleteNodeByKey(self):
+        pass
+
+    def test_Count(self):
+        pass
