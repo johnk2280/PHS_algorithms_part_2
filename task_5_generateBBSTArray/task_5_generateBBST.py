e83f708035ae73db185e2524274a5a6c96d66b11
johnk2280 <johnk2280@gmail.com>
2021-10-05 20:49:20 +0500

    [add] added task_5 branch

---
+++ task_5_generateBBSTArray/task_5_generateBBST.py
@@ -0,0 +1,27 @@
+class aBST:
+
+    def __init__(self, depth):
+        tree_size = pow(2, depth + 1) - 1
+        self.Tree = [None] * tree_size
+
+    def FindKeyIndex(self, key):
+        index = 0
+        while index < len(self.Tree):
+            if self.Tree[index] is None:
+                return 0 if index == 0 else -index
+            elif key == self.Tree[index]:
+                return index
+            elif key > self.Tree[index]:
+                index = (index + 1) * 2
+            else:
+                index = index * 2 + 1
+
+        return None
+
+    def AddKey(self, key):
+        index = self.FindKeyIndex(key)
+        if index is not None:
+            self.Tree[abs(index)] = key
+            return abs(index)
+
+        return -1
