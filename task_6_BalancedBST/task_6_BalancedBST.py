17ec7d60ea83c9cb0de9ed6761766920ab02cc63
johnk2280 <johnk2280@gmail.com>
2021-10-17 22:41:51 +0500

    [add] added solution

--- task_6_BalancedBST/task_6_BalancedBST.py
+++ task_6_BalancedBST/task_6_BalancedBST.py
@@ -13,21 +13,25 @@ class BalancedBST:
     def __init__(self):
         self.Root = None  # корень дерева
 
-    def GenerateTree(self, a, parent=None):
+    def GenerateTree(self, a, parent=None, level=0):
+        level += 1 if parent else 0
         if len(a) < 1:
             return
         elif len(a) == 1:
-            return BSTNode(a[0], parent)
+            leaf = BSTNode(a[0], parent)
+            leaf.Level = level
+            return leaf
 
         a.sort()
         root_index = len(a) // 2
+        root_index = a.index(a[root_index])
         sub_tree_root = BSTNode(a[root_index], parent)
-
         if not parent:
             self.Root = sub_tree_root
 
-        sub_tree_root.LeftChild = self.GenerateTree(a[: root_index], sub_tree_root)
-        sub_tree_root.RightChild = self.GenerateTree(a[root_index + 1:], sub_tree_root)
+        sub_tree_root.Level = level
+        sub_tree_root.LeftChild = self.GenerateTree(a[: root_index], sub_tree_root, level)
+        sub_tree_root.RightChild = self.GenerateTree(a[root_index + 1:], sub_tree_root, level)
         return sub_tree_root
 
     def IsBalanced(self, root_node):
