e651bf641329577846ba71a6e41f606b41426f96
johnk2280 <johnk2280@gmail.com>
2021-10-19 22:10:46 +0500

    [add] added lesson_7 branch

---
+++ task_7_heap/task_7_heap.py
@@ -0,0 +1,17 @@
+class Heap:
+
+    def __init__(self):
+        self.HeapArray = []  # хранит неотрицательные числа-ключи
+
+    def MakeHeap(self, a, depth):
+        # создаём массив кучи HeapArray из заданного
+        # размер массива выбираем на основе глубины depth
+        pass
+
+    def GetMax(self):
+        # вернуть значение корня и перестроить кучу
+        return -1  # если куча пуста
+
+    def Add(self, key):
+        # добавляем новый элемент key в кучу и перестраиваем её
+        return False  # если куча вся заполнена
