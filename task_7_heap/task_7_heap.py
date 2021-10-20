7511919aa41c7f55ff203f6d66992fef9e84ad74
johnk2280 <johnk2280@gmail.com>
2021-10-20 16:49:47 +0500

    [add] added code

--- task_7_heap/task_7_heap.py
+++ task_7_heap/task_7_heap.py
@@ -1,17 +1,48 @@
 class Heap:
 
     def __init__(self):
-        self.HeapArray = []  # хранит неотрицательные числа-ключи
+        self.HeapArray = []
 
     def MakeHeap(self, a, depth):
-        # создаём массив кучи HeapArray из заданного
-        # размер массива выбираем на основе глубины depth
-        pass
+        heap_size = pow(2, depth + 1) - 1
+        self.HeapArray = [None] * heap_size
+        for key in a[: heap_size]:
+            self.Add(key)
 
     def GetMax(self):
+        if self.HeapArray[0] is None:
+            return -1  # если куча пуста
+
         # вернуть значение корня и перестроить кучу
-        return -1  # если куча пуста
 
     def Add(self, key):
-        # добавляем новый элемент key в кучу и перестраиваем её
-        return False  # если куча вся заполнена
+        if key is not None:
+            new_key_index = self._get_index()
+            try:
+                self.HeapArray[new_key_index] = key
+            except TypeError:
+                return False
+
+            if new_key_index > 0:
+                self._sift_up(new_key_index)
+                return True
+
+        return False
+
+    def _get_index(self):
+        try:
+            return self.HeapArray.index(None)
+        except ValueError:
+            return None
+
+    def _sift_up(self, index):
+        if index < 1:
+            return
+
+        parent_index = (index - 1) // 2
+        if self.HeapArray[parent_index] < self.HeapArray[index]:
+            self.HeapArray[parent_index], self.HeapArray[index] = self.HeapArray[index], self.HeapArray[parent_index]
+            self._sift_up(parent_index)
+
+    def _sift_down(self, index):
+        pass
---
+++ task_7_heap/test_task_7.py
@@ -0,0 +1,68 @@
+from task_7_heap import Heap
+
+
+def test_MakeHeap():
+    a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 11]
+    depth = 3
+    h = Heap()
+    h.MakeHeap(a, depth)
+
+    assert len(h.HeapArray) == 15
+    assert h.HeapArray == [11, 9, 6, 7, 8, 2, 5, 1, 4, 3, None, None, None, None, None]
+
+    depth_1 = 2
+    h_1 = Heap()
+    h_1.MakeHeap(a, depth_1)
+    assert len(h_1.HeapArray) == 7
+    assert h_1.HeapArray == [7, 4, 6, 1, 3, 2, 5]
+
+    h_2 = Heap()
+    h_2.MakeHeap([], depth_1)
+    assert len(h_2.HeapArray) == 7
+    assert h_2.HeapArray == [None, None, None, None, None, None, None]
+
+    h_3 = Heap()
+    h_3.MakeHeap(a, 0)
+    assert len(h_3.HeapArray) == 1
+    assert h_3.HeapArray == [1]
+
+
+def test_Add():
+    a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 11]
+    h = Heap()
+    h.MakeHeap(a, 3)
+
+    assert len(h.HeapArray) == 15
+    assert h.HeapArray == [11, 9, 6, 7, 8, 2, 5, 1, 4, 3, None, None, None, None, None]
+
+    assert h.Add(55) is True
+    assert h.HeapArray == [55, 11, 6, 7, 9, 2, 5, 1, 4, 3, 8, None, None, None, None]
+
+    assert h.Add(17) is True
+    assert h.HeapArray == [55, 11, 17, 7, 9, 6, 5, 1, 4, 3, 8, 2, None, None, None]
+
+    assert h.Add(27) is True
+    assert h.HeapArray == [55, 11, 27, 7, 9, 17, 5, 1, 4, 3, 8, 2, 6, None, None]
+
+    assert h.Add(67) is True
+    assert h.HeapArray == [67, 11, 55, 7, 9, 17, 27, 1, 4, 3, 8, 2, 6, 5, None]
+
+    assert h.Add(21) is True
+    assert h.HeapArray == [67, 11, 55, 7, 9, 17, 27, 1, 4, 3, 8, 2, 6, 5, 21]
+
+    assert h.Add(221) is False
+    assert h.HeapArray == [67, 11, 55, 7, 9, 17, 27, 1, 4, 3, 8, 2, 6, 5, 21]
+
+    h_1 = Heap()
+    h_1.MakeHeap(a, 2)
+    assert h_1.Add(None) is False
+    assert h_1.HeapArray == [7, 4, 6, 1, 3, 2, 5]
+
+    h_2 = Heap()
+    h_2.MakeHeap(a, 3)
+    assert h_2.Add(None) is False
+    assert h_2.HeapArray == [11, 9, 6, 7, 8, 2, 5, 1, 4, 3, None, None, None, None, None]
+
+
+
+
