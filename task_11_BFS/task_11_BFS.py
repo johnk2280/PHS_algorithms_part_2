d13e36a006b0e9a46119e6316c6ef3b2d6e4efae
johnk2280 <johnk2280@gmail.com>
2021-10-29 15:50:52 +0500

    [add] added solution

--- task_11_BFS/task_11_BFS.py
+++ task_11_BFS/task_11_BFS.py
@@ -84,15 +84,35 @@ class SimpleGraph:
             return
 
     def BreadthFirstSearch(self, VFrom, VTo):
+        search_result = []
+        current_vertex = None
         self._clear_visit_flag()
-        queue = self._set_queue(VFrom)
-        parents = self._set_parents(VFrom)
-        self.vertex[VFrom].Hit = True
 
-        # узлы задаются позициями в списке vertex
-        # возвращается список узлов -- путь из VFrom в VTo
-        # или [] если пути нету
-        pass
+        try:
+            queue = self._set_queue(VFrom)
+            parents = self._set_parents(VFrom)
+            self.vertex[VFrom].Hit = True
+            while queue:
+                current_vertex = queue.pop(0)
+                self.vertex[current_vertex].Hit = True
+                parents = self._set_parents(current_vertex, parents)
+                if current_vertex == VTo:
+                    break
+                elif self.m_adjacency[current_vertex][VTo]:
+                    search_result.append(self.vertex[current_vertex])
+                    break
+                else:
+                    queue = self._set_queue(current_vertex, queue)
+
+            search_result.append(self.vertex[VTo])
+            parent = parents[current_vertex]
+            while parent is not None:
+                search_result.insert(0, self.vertex[parent])
+                parent = parents[parent]
+
+            return search_result
+        except (IndexError, TypeError) as errors:
+            return
 
     def _clear_visit_flag(self):
         for v in self.vertex:
@@ -103,7 +123,9 @@ class SimpleGraph:
             queue = []
 
         neighbors = self.m_adjacency[VFrom]
-        queue.extend([i for i in range(len(neighbors)) if neighbors[i]])
+        queue.extend([
+            i for i in range(len(neighbors)) if neighbors[i] and not self.vertex[i].Hit
+        ])
         return queue
 
     def _set_parents(self, VFrom: int, parents: list = None) -> list:
@@ -112,7 +134,7 @@ class SimpleGraph:
 
         neighbors = self.m_adjacency[VFrom]
         for i in range(len(neighbors)):
-            if neighbors[i]:
+            if neighbors[i] and not self.vertex[i].Hit:
                 parents[i] = VFrom
 
         return parents
--- task_11_BFS/test_task_11.py
+++ task_11_BFS/test_task_11.py
@@ -2,31 +2,92 @@ from task_11_BFS import Vertex, SimpleGraph
 
 
 def test_DepthFirstSearch():
-    g = SimpleGraph(3)
+    g = SimpleGraph(6)
 
-    assert g.max_vertex == 3
+    assert g.max_vertex == 6
     assert g.m_adjacency == [
-        [0, 0, 0],
-        [0, 0, 0],
-        [0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0],
     ]
-    assert g.vertex == [None, None, None]
+    assert g.vertex == [None, None, None, None, None, None]
 
-    for i in range(3):
+    for i in range(6):
         g.AddVertex(i)
 
-    assert g.m_adjacency == [
-        [0, 0, 0],
-        [0, 0, 0],
-        [0, 0, 0],
-    ]
-
     g.AddEdge(0, 1)
-    g.AddEdge(1, 2)
+    g.AddEdge(0, 2)
+    g.AddEdge(1, 3)
+    g.AddEdge(1, 4)
+    g.AddEdge(5, 4)
     assert g.m_adjacency == [
-        [0, 1, 0],
-        [1, 0, 1],
-        [0, 1, 0],
+        [0, 1, 1, 0, 0, 0],
+        [1, 0, 0, 1, 1, 0],
+        [1, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 1],
+        [0, 0, 0, 0, 1, 0]
     ]
 
-    assert g.DepthFirstSearch(0, 2) == [g.vertex[0], g.vertex[1], g.vertex[2]]
+    assert g.DepthFirstSearch(0, 5) == [
+        g.vertex[0],
+        g.vertex[1],
+        g.vertex[4],
+        g.vertex[5],
+    ]
+
+    assert g.BreadthFirstSearch(0, 5) == [
+        g.vertex[0],
+        g.vertex[1],
+        g.vertex[4],
+        g.vertex[5],
+    ]
+
+    g.AddEdge(2, 5)
+    g.AddEdge(3, 5)
+    assert g.m_adjacency == [
+        [0, 1, 1, 0, 0, 0],
+        [1, 0, 0, 1, 1, 0],
+        [1, 0, 0, 0, 0, 1],
+        [0, 1, 0, 0, 0, 1],
+        [0, 1, 0, 0, 0, 1],
+        [0, 0, 1, 1, 1, 0]
+    ]
+
+    assert g.DepthFirstSearch(0, 5) == [
+        g.vertex[0],
+        g.vertex[1],
+        g.vertex[3],
+        g.vertex[5],
+    ]
+
+    assert g.BreadthFirstSearch(0, 5) == [
+        g.vertex[0],
+        g.vertex[2],
+        g.vertex[5],
+    ]
+
+    assert g.DepthFirstSearch(4, 0) == [
+        g.vertex[4],
+        g.vertex[1],
+        g.vertex[0],
+    ]
+
+    assert g.BreadthFirstSearch(4, 0) == [
+        g.vertex[4],
+        g.vertex[1],
+        g.vertex[0],
+    ]
+
+    assert g.DepthFirstSearch(4, 5) == [
+        g.vertex[4],
+        g.vertex[5],
+    ]
+
+    assert g.BreadthFirstSearch(4, 5) == [
+        g.vertex[4],
+        g.vertex[5],
+    ]
